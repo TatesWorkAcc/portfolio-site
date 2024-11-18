307bbff4e476f339d30a83a4a590eecc6a6a1a72
Brandon Tate <tatesworkacc@gmail.com>
2024-11-18 14:57:28 -0600

    added header, links, project button

---
+++ src/Header.jsx
@@ -0,0 +1,18 @@
+
+function Header(){
+
+    return(
+        <div className="header-container">
+            <div className="header-grid">
+                <button className="link header-button"> <a href="https://github.com/TatesWorkAcc">My Github</a></button>
+                <button className="link header-button"><a href="https://www.linkedin.com/in/brandon-tate-2bb32b2b1/">My Linkedin</a></button>
+            </div>
+            <div className="header-grid">
+                <button className="header-button">Projects</button>
+            </div>
+        </div>
+    )
+
+}
+
+export default Header
