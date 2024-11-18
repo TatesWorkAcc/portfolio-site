758465975aad66fbc7219c3106b09ede349125ac
Brandon Tate <tatesworkacc@gmail.com>
2024-11-18 15:07:26 -0600

    added name text

--- src/Name.jsx
+++ src/Name.jsx
@@ -3,7 +3,8 @@ function Name(){
 
     return(
         <div className="name-container">
-            
+            <h1 className="name-text">Brandon Tate</h1>
+            <h2 className="name-text">Front End Developer</h2>
         </div>
     )
 
