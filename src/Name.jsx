8af6bd6aba8e7e37950750b3a9d84dc2991ebd7f
Brandon Tate <tatesworkacc@gmail.com>
2024-11-18 15:10:13 -0600

    changed classname

--- src/Name.jsx
+++ src/Name.jsx
@@ -4,7 +4,7 @@ function Name(){
     return(
         <div className="name-container">
             <h1 className="name-text">Brandon Tate</h1>
-            <h2 className="name-text">Front End Developer</h2>
+            <h2 className="profession-text">Front End Developer</h2>
         </div>
     )
 
