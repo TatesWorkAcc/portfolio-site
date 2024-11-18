06b5088ead52ed9669469b5b6c104fcd3ed4fa17
Brandon Tate <tatesworkacc@gmail.com>
2024-11-18 16:56:26 -0600

    adding skills

--- src/Name.jsx
+++ src/Name.jsx
@@ -2,10 +2,21 @@
 function Name(){
 
     return(
-        <div className="name-container">
-            <h1 className="intro name-text font-text">Brandon Tate</h1>
-            <h2 className="intro profession-text font-text">Front End Developer</h2>
-        </div>
+        <>
+            <div className="name-container">
+                <h1 className="intro name-text font-text">Brandon Tate</h1>
+                <h2 className="intro profession-text font-text">Front End Developer</h2>
+                <div className="skill-container">
+                    <h3 className="intro skills-text font-text">Skills</h3>
+                    <div className="skill-grid">
+                    <h3>Languages:</h3> <h3>C++, JavaScript, Python</h3>
+                    <h3>Web Development:</h3> <h3>HTML, CSS, React, Responsive Design</h3>
+                    <h3>Framework & Tools:</h3> <h3>State Management, Prettier, Eslint, Git</h3>
+                    </div>
+                </div>
+            </div>
+            
+        </>
     )
 
 }
