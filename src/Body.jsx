a62c63367592084c66720d75c60a751cbed0783d
Brandon Tate <tatesworkacc@gmail.com>
2024-11-19 02:57:29 -0600

    changed text

--- src/Body.jsx
+++ src/Body.jsx
@@ -49,22 +49,22 @@ function Body() {
             </a>
           </div>
           <p className="project-text">
-            Get users input for a topic and displays news about the topic in an
-            orderly fashion (title, image, author, date, description, content,
-            and url). <br></br>
-            <br></br>● Built a news search app using React, fetching articles
-            from the NewsAPI based on user-defined queries.
+            A news search app that allows users to input a topic and displays
+            relevant articles in an organized format, including title, image,
+            author, date, description, content, and URL. <br></br>
+            <br></br>● Built the app using React and NewsAPI, fetching articles
+            based on user-defined queries.
             <br></br>
-            <br></br>● Integrated React Hooks (useState, useEffect) to manage
-            API calls and dynamic state updates.<br></br>
-            <br></br>● Displayed article data such as title, image, author,
-            publish date, description, content, and provided clickable links to
-            full articles.<br></br>
-            <br></br>● Implemented responsive CSS styling to ensure the web
-            design pages have correct scalability across devices.
+            <br></br>● Integrated React Hooks (useState, useEffect) for API
+            calls and dynamic state management.<br></br>
+            <br></br>● Displayed detailed article data, including clickable
+            links to full articles.<br></br>
+            <br></br>● Implemented responsive CSS for mobile-friendly design
+            across devices.
             <br></br>
-            <br></br>● Improved user experience by filtering out incomplete
-            articles and invalid author data.<br></br>
+            <br></br>● Enhanced user experience by filtering out incomplete
+            articles and invalid author data.
+            <br></br>
           </p>
         </motion.div>
         <motion.div
@@ -93,18 +93,20 @@ function Body() {
             </a>
           </div>
           <p className="project-text">
-            Gets user input for what city they live in, gives today and the next
-            5 days temp, condition, description, and humidity <br></br>
-            <br></br>● Designed a weather forecasting app using React and
-            OpenWeatherMap API to provide real-time weather updates. <br></br>
-            <br></br>● Implemented a search feature to fetch data based on city
-            input, displaying current conditions and a 5-day forecast. <br></br>
-            <br></br>● Displayed key metrics such as temperature, conditions,
-            humidity, and descriptions in a user-friendly format. <br></br>
-            <br></br>● Default data for New York City ensured a functional
-            interface upon load. <br></br>
-            <br></br>● Utilized responsive design principles for mobile and
-            desktop views. <br></br>
+            A weather app that provides current and 5-day forecasts based on the
+            user's input for their city, displaying key metrics like
+            temperature, conditions, and humidity. <br></br>
+            <br></br>● Developed using React and OpenWeatherMap API to fetch
+            real-time weather data. <br></br>
+            <br></br>● Enabled users to search weather by city and view a 5-day
+            forecast. <br></br>
+            <br></br>● Displayed temperature, weather conditions, humidity, and
+            descriptions in a clear, user-friendly interface. <br></br>
+            <br></br>● Ensured functionality with default weather data for New
+            York City. <br></br>
+            <br></br>● Applied responsive design principles to ensure
+            compatibility across mobile and desktop views.
+            <br></br>
             <br></br>
           </p>
         </motion.div>
@@ -136,16 +138,17 @@ function Body() {
             </a>
           </div>
           <p className="project-text">
-            Enter your income and your expenses, get your total amount of income
-            after expenses have been paid. <br></br>
+            A financial tracker app where users can input their income and
+            expenses to see their total income after expenses are deducted.{" "}
+            <br></br>
             <br></br>● Developed a financial tracking tool using React to manage
             income and expenses.<br></br>
-            <br></br>● Incorporated state management for real-time updates of
+            <br></br>● Incorporated state management to dynamically update
             financial categories (e.g., housing, transportation, food).<br></br>
             <br></br>● Created reusable functions such as handleTotalIncome and
             handleAfterExpenses for dynamic calculations.<br></br>
-            <br></br>● Employed Prettier and ESLint to maintain clean and
-            error-free code formatting.
+            <br></br>● Used Prettier and ESLint to maintain clean and error-free
+            code formatting.
           </p>
         </motion.div>
       </div>
